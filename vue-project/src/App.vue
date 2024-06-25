64c0e7e072b501fd92e639a09c34adbb59b60c2b
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2024-06-25 18:26:42 +0300

    add_button

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,21 +1,38 @@
-<script> 
-import PriceDisplay from './components/PriceDisplay.vue';
-
-export default {
-  components: {
-    PriceDisplay
-  }
-};
-</script>
-
 <template>
-    <div>
+  <div>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+  <button type="button" @click="this.info = 'Some new'">Отправить</button>
+  </div>
+  <div>
     <PriceDisplay :price="1000" />
     <PriceDisplay :price="5000" />
     <!-- Добавьте столько компонентов PriceDisplay, сколько нужно -->
   </div>
 </template>
 
-<style scoped>
+<script>
+import PriceDisplay from './components/PriceDisplay.vue'
 
+export default {
+  components: {
+    PriceDisplay
+  },
+  data() {
+    return {
+      info: 'Title',
+      someInfo: 'Anons of message'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 { 
+  font-weight: lighter; 
+}
+
+p {
+color: red;
+}
 </style>
